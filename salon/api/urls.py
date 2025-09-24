f11141b445eecc7fd4835e32861b159d302a21d6
Liya Yonas <lyonas@charlotte.edu>
2025-09-24 14:43:32 -0400

    SALON-13: Create Appointment model in api app

--- salon/api/urls.py
+++ salon/api/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-
+from .views import AppointmentViewSet
 router = routers.SimpleRouter()
 
 urlpatterns = [
